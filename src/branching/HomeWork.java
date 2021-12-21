157ac2d7098b2b3379fc23f30287758a17ec3b90
Adam Abshire <chef.abshire@gmail.com>
2021-12-21 17:01:15 -0500

    cleaned up the console

--- src/branching/HomeWork.java
+++ src/branching/HomeWork.java
@@ -2,9 +2,17 @@ package branching;
 
 public class HomeWork {
 	public static void main(String[] args) {
-
-		// Question 1
-		String colorOption = "Red";
+		/**
+		 Question 1:
+		 You are given a String variable colorOption. Your task is to dsplay in console:
+		 	*"Red color" if colorOption is equal to "Red"
+		 	*"Orange Color" if colorOption is equal to "Orange"
+		 	*"Black color" if colorOption is equal to "Black"
+		 	*"Wrong color, not available" if colorOption is none of the above
+		 */
+		
+		// Question 1 Switch
+		String colorOption = "Orange";
 		String colorChoice;
 		switch (colorOption) {
 		case "Red":
@@ -20,9 +28,10 @@ public class HomeWork {
 			colorChoice = "Wrong color, not available";
 			break;
 		}
-		System.out.println(colorChoice);
-		System.out.println("****************");
-
+		System.out.println("Question 1 :: Choosing a Color");
+		System.out.println("Switch: " + colorChoice);
+		
+		// Question 1 If-Else
 		if (colorOption == "Red") {
 			colorChoice = "Red color";
 		} else if (colorOption == "Orange") {
@@ -32,11 +41,18 @@ public class HomeWork {
 		} else {
 			colorChoice = "Wrong color, not availbable";
 		}
-		System.out.println(colorChoice);
-		System.out.println("*****************");
-
-		// Question 2
-		char gender = 'f';
+		System.out.println("If Else: " + colorChoice);
+		System.out.println("********************************");
+		
+		/**
+		 Question 2:
+		 Declare and initialize char gender (M, m, F, f) and print the full gender (Female, Male) using
+		 a. switch statement
+		 b. if else statement
+		 */
+		
+		// Question 2 Switch
+		char gender = 'M';
 		String genderChoice;
 		switch (gender) {
 		case 'm':
@@ -55,9 +71,10 @@ public class HomeWork {
 			genderChoice = "Not a gender... please select m or f";
 			break;
 		}
-		System.out.println(genderChoice);
-		System.out.println("**************");
-
+		System.out.println("Question 2 :: Choosing a Gender");
+		System.out.println("Switch: " + genderChoice);
+		
+		// Question 2 If-Else
 		if (gender == 'f') {
 			genderChoice = "Female";
 		} else if (gender == 'F') {
@@ -69,19 +86,19 @@ public class HomeWork {
 		} else {
 			genderChoice = "Not a gender.. please select m or f";
 		}
-		System.out.println(genderChoice);
-		System.out.println("***************");
+		System.out.println("If Else: " + genderChoice);
+		System.out.println("********************************");
 
-		// Question 3
-		int number = 0;
-		String isOdd;
-		if (number % 2 == 0) {
-			isOdd = "Even Number";
-		} else {
-			isOdd = "Odd Number";
-		}
-		System.out.println(isOdd);
-		System.out.println("**************");
+		/**
+		 Question 3:
+		 Declare and initialize int number. Check whether the number is EVEN or ODD using
+		 a. Switch statement
+		 b. If else statement
+		 */
+		
+		// Question 3 Switch
+		int number = 3;
+		String isOdd = "";
 
 		switch (number % 2) {
 		case 0:
@@ -91,6 +108,16 @@ public class HomeWork {
 			isOdd = "Odd Number";
 			break;
 		}
-		System.out.println(isOdd);
+		System.out.println("Question 3 :: Even or Odd");
+		System.out.println("Switch: " + isOdd);
+
+		// Question 3 If-Else
+		if (number % 2 == 0) {
+			isOdd = "Even Number";
+		} else {
+			isOdd = "Odd Number";
+		}
+		System.out.println("If Else: " + isOdd);
+		System.out.println("********************************");
 	}
 }
